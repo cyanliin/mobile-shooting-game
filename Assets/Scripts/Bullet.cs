28a8e3e0cd04440dfb12c8928413ae7e31b59c3f
Grass Chen <grass527@gmail.com>
2022-11-02 12:22:43 +0800

    feat: 敵人扣血改為子彈自身的攻擊力

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 30f;
+    public float atk = 50f;
     private Rigidbody rb;
 
     void Start()
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,13 +6,11 @@ public class Enemy : MonoBehaviour
 {
     private float hp = 100f;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -23,8 +21,11 @@ public class Enemy : MonoBehaviour
         // 如果碰撞到的是子彈
         if (other.tag == "Bullet")
         {
+            // 先取得子彈的攻擊力
+            Bullet bullet = other.GetComponent<Bullet>();
+
             // 先扣血
-            hp -= 25;
+            hp -= bullet.atk;
 
             // 如果沒血了，就刪除自己
             if (hp <= 0)
